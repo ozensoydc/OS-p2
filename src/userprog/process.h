ffe083f11db808d07cebbd08fd3f41fc02db2eaa
ozensoydc <ozensoydc@uchicago.edu>
2014-11-01 12:53:47 -0500

    not like you really need the header files now right?

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -7,5 +7,6 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
-
+void* where_is_null(void* v_addr);
+void* revert_byte_order(void* v_addr, int to_revert);
 #endif /* userprog/process.h */
