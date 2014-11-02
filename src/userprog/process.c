6b1659d9017b67a57f748b15c4a17c796f4230d1
ozensoydc <ozensoydc@uchicago.edu>
2014-11-02 01:15:07 -0500

    working with child threads

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -33,18 +33,24 @@ process_execute (const char *file_name)
 {
   char *fn_copy;
   tid_t tid;
-
+  struct thread* p_thread = thread_current();
+  
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
+    //sema_up(p_thread->child_lock);
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
-  if (tid == TID_ERROR)
+  sema_down(p_thread->child_lock);
+  if (tid == TID_ERROR){
     palloc_free_page (fn_copy); 
+  }
+  
+  sema_up(p_thread->child_lock);
   return tid;
 }
 
